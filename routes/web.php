b51ff5009ee736feb251550545b5727fe821bcdd
Raihanul Haque <raihancse101@gmail.com>
2025-07-29 16:59:56 +0600

    Admin and font file add,welcome controller added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Fontend\welcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+//fontend routes
+Route::get('/',[welcomeController::class, 'index'])->name('welcome');
